af5b60bd93902437e038253b335656c93149055f
oiwa-h <oiwa-h@gnavi.co.jp>
2014-05-30 09:55:54 +0900

    Initial Commit

---
+++ hello-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hello-iOS
+//
+//  Created by oiwa-h on 2014/05/30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hello-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hello-iOS
+//
+//  Created by oiwa-h on 2014/05/30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
